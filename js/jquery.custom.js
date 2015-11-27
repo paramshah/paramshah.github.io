19dee3532108e4e087a1efc8325f9eea1f4ee656
Alex Mathews <amathew9@jhu.edu>
2015-11-27 12:06:37 -0500

    starting d3

--- js/jquery.custom.js
+++ js/jquery.custom.js
@@ -10,7 +10,6 @@
 		$('.parallax .parallax-banner-1').parallax("50%", 0.2);
 		$('.newsletter').parallax("50%", 0.2);
 		
-        
         $(document).on("scroll", onScroll);
 		/* Scroll to Main
 		================================================== */
@@ -46,26 +45,49 @@
 		});
 		
 		
+		/* Flexslider with Video
+		================================================== */		
+		$(window).load(function(){
+			
+			// Vimeo API nonsense
+		  	var player = document.getElementById('player_1');
+		  
+			function addEvent(element, eventName, callback) {
+				(element.addEventListener) ? element.addEventListener(eventName, callback, false) : element.attachEvent(eventName, callback, false);
+			}
+			
+			$('.flexslider').fitVids().flexslider({
+				animation: "slide",
+				controlNav: "thumbnails",
+				smoothHeight: true,
+				start: function(slider){
+			  		$('body').removeClass('loading');
+				},
+				before: function(slider){
+					$f(player).api('pause');
+			  	}
+		  	});
+			
+		});
+		
+		 function onScroll(event){
+        
+            var scrollPos = $(document).scrollTop();
+            $('#ut-navigation a').each(function () {
+                var currLink = $(this);
+                var refElement = $(currLink.attr("href"));
+                if (refElement.position().top <= scrollPos + 80 && refElement.position().top + refElement.height() > scrollPos+ 80) {
+                    $('#ut-navigationli a').removeClass("selected");
+                    currLink.addClass("selected");
+                }
+                else{
+                    currLink.removeClass("selected");
+                }
+            });
+
+        }
 	});
 
-    
-    function onScroll(event){
-        
-    var scrollPos = $(document).scrollTop();
-    $('#ut-navigation a').each(function () {
-        var currLink = $(this);
-		var refElement = $(currLink.attr("href"));
-        if (refElement.position().top <= scrollPos + 80 && refElement.position().top + refElement.height() > scrollPos+ 80) {
-            $('#ut-navigationli a').removeClass("selected");
-            currLink.addClass("selected");
-        }
-        else{
-            currLink.removeClass("selected");
-        }
-    });
-    
-}
-      
 })(jQuery);
 
 new cbpScroller( document.getElementById( 'mainsite' ) );
